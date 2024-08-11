3894c240f16bd472e72b98739eece581c1ca4551
lihaixin <lihaixin@15099.net>
2024-08-11 21:44:33 +0800

    Update merge_json_files.sh

--- merge_json_files.sh
+++ merge_json_files.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
-
+cd /usr/share/nginx/html/
 # 设定目标合并文件的名称
-output_file="merged_files.json"
+output_file="index.json"
 
 # 初始化该文件（如果存在则清空内容）
 > "$output_file"
+# 添加创建头部规则
+cat >> $output_file<< TEMPEOF
+{
+	"version": "2",
+	"templates": [
+TEMPEOF
+fi
 
 # 使用find命令查找当前目录下所有子目录中的.json文件
 # 然后使用cat命令将它们的内容追加到输出文件中
-find . -type f -name "*.json" -not -path "./merged_files.json" | while read file; do
+find . -type f -name "*.json" -not -path "./index.json" | while read file; do
     cat "$file" >> "$output_file"
 done
 
+# 添加路由尾部
+sed -i '$d' $output_file
+sleep 1
+cat >> $output_file<< TEMPEOF       
+	]
+}
+TEMPEOF
 echo "合并完成，所有.json文件内容已保存至 $output_file"
