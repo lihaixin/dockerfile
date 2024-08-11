43af3672d3c5f28bdf4538e82fd60be8cbea17c8
lihaixin <lihaixin@15099.net>
2024-08-12 00:49:10 +0800

    Update merge_json_files.sh

--- merge_json_files.sh
+++ merge_json_files.sh
@@ -15,7 +15,7 @@ TEMPEOF
 
 # 使用find命令查找当前子目录中的.json文件
 # 然后使用cat命令将它们的内容追加到输出文件中
-find . -type f -name "*.json" -not -path "./index.json" | while read file; do
+find . -type f \( -name "1.json" -o -name "2.json" -o -name "3.json" \) ! -path "./index.json" | while read file; do
     cat "$file" >> "$output_file"
 done
 
