8203ca15166bb89ce00c8b0f2d9f3a30a62da60f
lihaixin <lihaixin@15099.net>
2024-08-12 23:19:27 +0800

    Update merge_json_files.sh

--- merge_json_files.sh
+++ merge_json_files.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
-cd /usr/share/nginx/html/
-# 设定目标合并文件的名称
-output_file="index.json"
 
-# 初始化该文件（如果存在则清空内容）
-> "$output_file"
-# 添加创建头部规则
-cat >> $output_file<< TEMPEOF
+merge_json_files(){
+    cd /usr/share/nginx/html/
+    
+    # 输出文件名作为第一个参数，如果没有提供，默认为"index.json"
+    output_file="${1:-index.json}"
+
+    # 如果提供了第二个参数及以后，视为要合并的文件名
+    if [ "$#" -gt 1 ]; then
+        files=("${@:2}") # 从第二个参数开始，获取所有要合并的文件名
+    else
+        # 默认合并的文件列表
+        files=("1.json" "2.json" "3.json")
+    fi
+
+    # 初始化该文件（如果存在则清空内容）
+    > "$output_file"
+    # 添加创建头部规则
+    cat >> "$output_file"<< TEMPEOF
 {
 	"version": "2",
 	"templates": [
 TEMPEOF
 
+    for file_pattern in "${files[@]}"; do
+        # 使用find命令查找匹配的.json文件
+        find . -type f -name "$file_pattern" ! -path "./$output_file" | while read file; do
+            cat "$file" >> "$output_file"
+        done
+    done
 
-# 使用find命令查找当前子目录中的.json文件
-# 然后使用cat命令将它们的内容追加到输出文件中
-find . -type f \( -name "1.json" -o -name "2.json" -o -name "3.json" \) ! -path "./index.json" | while read file; do
-    cat "$file" >> "$output_file"
-done
-
-# 添加路由尾部
-sed -i '$d' $output_file
-sleep 1
-cat >> $output_file<< TEMPEOF       
+    # 添加路由尾部
+    sed -i '$d' "$output_file"
+    sleep 1
+    cat >> "$output_file"<< TEMPEOF       
 	]
 }
 TEMPEOF
-echo "合并完成，所有.json文件内容已保存至 $output_file"
+    echo "合并完成，所有.json文件内容已保存至 $output_file"
+}
+
+# 示例用法：
+# 合并默认文件到默认输出文件名
+merge_json_files
+
+# 合并特定文件到指定输出文件
+merge_json_files "index3.json" "3.json"
