47088427a1a76da4d12fa2fc1ee391c9bbd3d9ef
lihaixin <lihaixin@15099.net>
2024-09-08 12:17:30 +0800

    Update convert_md_to_html.sh

--- convert_md_to_html.sh
+++ convert_md_to_html.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 # 定义转换函数
-convert_md_to_html() {
-    local md_file="$1"
-    local dir_name=$(dirname "$md_file")
-    (cd "$dir_name" &&pandoc README.md -s --metadata charset=utf-8 --metadata title="README 指南" -o index.html)
-}
-
-
+local md_file="$1"
+local dir_name=$(dirname "$md_file")
+(cd "$dir_name" &&pandoc README.md -s --metadata charset=utf-8 --metadata title="README 指南" -o index.html)
