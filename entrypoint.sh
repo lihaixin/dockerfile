5f8bdd6c9e3206df9f19569e4f6d85c1b9c7fda4
lihaixin <lihaixin@15099.net>
2024-09-08 11:43:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 merge_json_files.sh
-pandoc /usr/share/nginx/html/README.md -s --metadata charset=utf-8 --metadata title="README 指南" -o /usr/share/nginx/html/index.html
+convert_md_to_html.sh
+# pandoc /usr/share/nginx/html/README.md -s --metadata charset=utf-8 --metadata title="README 指南" -o /usr/share/nginx/html/index.html
 nginx -g "daemon off;"
