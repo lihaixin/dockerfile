b83e614d8086e552604559465cc747da527b676a
lihaixin <lihaixin@15099.net>
2024-09-24 22:55:44 +0800

    Update docker-cli.sh

--- stacks/portainer/agent/docker-cli.sh
+++ stacks/portainer/agent/docker-cli.sh
@@ -4,10 +4,11 @@ docker run -d \
   -v /:/host \
   -v /var/portainer_agent_data:/data \
   --restart always \
+  -p 9001:9001 \
+  -p 9090:9090 \
   -e EDGE=0 \
   -e EDGE_ID=733c7ebf-6804-4604-ae41-a1444b6cec34 \
   -e EDGE_KEY=aHR0cHM6Ly91aS0yLjE1MDk5Lm5ldHx1aS0yLjE1MDk5Lm5ldDo4MDA4fDROaFAzd2lsOENIbkFLYzl6Mi9DUndGQkhJWGF1LzRlZnhpbVdzWlhBNXc9fDQ5NA \
   -e EDGE_INSECURE_POLL=1 \
   --name ui2 \
-  --network 1macvlan --ip=192.168.2.11 \
   lihaixin/portainer:agent-2.19.5 
