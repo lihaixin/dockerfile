ed5a97c12cdaa29c3fef679a34b071fad28e26d2
lihaixin <lihaixin@15099.net>
2024-09-24 22:10:52 +0800

    Update docker-cli.sh

--- stacks/portainer/agent/docker-cli.sh
+++ stacks/portainer/agent/docker-cli.sh
@@ -4,7 +4,7 @@ docker run -d \
   -v /:/host \
   -v /var/portainer_agent_data:/data \
   --restart always \
-  -e EDGE=1 \
+  -e EDGE=0 \
   -e EDGE_ID=733c7ebf-6804-4604-ae41-a1444b6cec34 \
   -e EDGE_KEY=aHR0cHM6Ly91aS0yLjE1MDk5Lm5ldHx1aS0yLjE1MDk5Lm5ldDo4MDA4fDROaFAzd2lsOENIbkFLYzl6Mi9DUndGQkhJWGF1LzRlZnhpbVdzWlhBNXc9fDQ5NA \
   -e EDGE_INSECURE_POLL=1 \
