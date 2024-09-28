f6ba9996bd86619b8505d26c57e1ad694cc25ae4
lihaixin <lihaixin@15099.net>
2024-09-28 09:39:09 +0800

    Update docker-cli.sh

--- stacks/portainer/ce/docker-cli.sh
+++ stacks/portainer/ce/docker-cli.sh
@@ -2,7 +2,7 @@ docker run -d \
 -p 9443:9443 \
 -p 8000:8000 \
 -v /var/run/docker.sock:/var/run/docker.sock \
--v /vat/portainer_data:/data \
+-v /var/portainer_data:/data \
 -e TPORT=8000 \
 -e COUNTRY=cn \
 -e PASSWORD="@china1234567" \
